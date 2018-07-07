9ad007d8513e34553ac281d8093efdfb2533b8c3
Parsa Ghadimi <me@qti3e.com>
2018-07-07 06:40:15 +0430

    Rewrite writer

--- website/bin.ts
+++ website/bin.ts
@@ -2,20 +2,19 @@ import * as fs from "fs";
 import { parse } from "./parser";
 import { Writer } from "./writer";
 
-// Read file and parse it.
-const fileName = process.argv[2];
-
-if (!fileName) {
-  console.log("Usage: ts-node bin.ts [fileName]");
+if (process.argv.length > 4 || process.argv.length < 3) {
+  console.log("Usage: ts-node bin.ts [--html] [fileName]");
   process.exit(1);
 }
 
+// Read file and parse it.
+const fileName = process.argv[process.argv.length === 3 ? 2 : 3];
+const isHTML = process.argv[process.argv.length === 4 ? 2 : null];
+
 const sourceCode = fs.readFileSync(fileName).toString();
 const doc = parse(sourceCode, fileName);
-const writer = new Writer("");
+const writer = new Writer(isHTML === "---html");
 
 for (const entity of doc) {
   writer.render(entity);
-  writer.eol();
-  writer.eol();
 }
--- website/writer.ts
+++ website/writer.ts
@@ -5,6 +5,13 @@ import chalk from "chalk";
 import * as r from "./renderers";
 import * as types from "./types";
 
+// Writer is the core part of documentation rendering,
+// it provides a single interface for other functions
+// to generate output for both HTML pages and CLI.
+
+// Pre compute renderers name.
+// Removes `render` from the begining of the name
+// and convert name to lowerCamelCase.
 const renderers = Object.create(null);
 for (let key in r) {
   if (r.hasOwnProperty(key)) {
@@ -15,80 +22,144 @@ for (let key in r) {
 }
 
 export class Writer {
-  private childWriter: Writer;
-  style = style;
-  constructor(private padding: string) {}
+  private style: Style;
+  private lastCharacter = "\n";
+  private padding = 0;
 
-  write(data: string) {
-    const lines = data.split(/\n/g).map((l, i) =>
-      (i === 0 ? "" : this.padding) + l
-    );
-    process.stdout.write(lines.join("\n"));
+  constructor(public isHTML: boolean) {
+    this.style = isHTML ? htmlStyle : cliStyle;
   }
 
-  eol() {
-    this.write("\n");
-  }
-
-  render(entity: types.SerilizedData | string) {
+  render(entity: string | types.SerilizedData): void {
     if (typeof entity === "string") {
-      this.writeDescription(entity + "\n");
+      this.text(entity);
       return;
     }
     const renderer = renderers[entity.type];
     if (!renderer) {
-      this.write(
-        style.error(
-          "error: Can not render `" + style.identifier(entity.type) + "`."
-        )
-      );
-      return;
+      throw new Error(`Can not render ${entity.type}.`);
+    }
+    if (this.isHTML) {
+      this.write(`<div class="doc-entity doc-${entity.type}"`);
     }
     renderer(this, entity);
+    if (this.isHTML) this.write(`</div>`);
   }
 
-  renderChild(entity: types.SerilizedData | string) {
-    if (!entity) return;
-    if (!this.childWriter) {
-      const padding = this.padding + "    ";
-      this.childWriter = new Writer(padding);
+  increasePadding() {
+    this.padding++;
+    if (this.isHTML) {
+      this.write(`<div class="padding">`);
     }
-    this.childWriter.eol();
-    this.childWriter.render(entity);
   }
 
-  renderParameters(parameters: type.SerilizedData[]) {
-    if (!parameters) return;
-    parameters.forEach((e, i) => {
-      if (i > 0 && parameters.length > 1) {
-        this.write(", ");
-      }
-      this.render(e);
-    });
+  decreasePadding() {
+    this.padding--;
+    if (this.padding < 0) {
+      throw new Error(
+        "Called decreasePadding() more than expected number of times."
+      );
+    }
+    if (this.isHTML) {
+      this.write(`</div>`);
+    }
   }
 
-  renderTypeParameters(parameters: type.TypeParameter[]) {
-    if (!parameters || parameters.length === 0) return;
-    this.write("<");
-    this.renderParameters(parameters);
-    this.write(">");
+  private cliWriteLine(line: string): void {
+    const padding = " ".repeat(this.padding * 4);
+    if (this.lastCharacter === "\n") {
+      line = padding + line;
+    }
+    process.stdout.write(line);
+    this.lastCharacter = line[line.length - 1];
   }
 
-  writeDescription(d: string) {
-    const tmp = this.padding;
-    const border = this.style.border("║ ");
-    this.padding += border;
-    this.write(border);
-    this.write(d);
-    this.padding = tmp;
+  private write(data: string): void {
+    if (this.isHTML) {
+      process.stdout.write(data);
+      return;
+    }
+    data.split(/\n/g).forEach(this.cliWriteLine.bind(this));
+  }
+
+  comemnt(str: string): void {
+    this.write(this.style.comment(str));
+  }
+
+  keyword(str: string): void {
+    this.write(this.style.comment(str));
+  }
+
+  identifier(str: string): void {
+    this.write(this.style.identifier(str));
+  }
+
+  literal(str: string): void {
+    this.write(this.style.literal(str));
+  }
+
+  text(str: string): void {
+    this.write(this.style.text(str));
+  }
+
+  anchor(str: string, href: string): void {
+    this.write(this.style.anchor(str, href));
+  }
+
+  pre(str: string): void {
+    this.write(this.style.pre(str));
+  }
+
+  header(str: string): void {
+    this.write(this.style.header(str));
   }
 }
 
-const style = {
-  border: chalk.red,
-  error: chalk.red.italic,
-  comment: chalk,
+export interface Style {
+  comment(str: string): string;
+  keyword(str: string): string;
+  identifier(str: string): string;
+  literal(str: string): string;
+  text(str: string): string;
+  anchor(str: string, href: string): string;
+  pre(str: string): string;
+  header(str: string): string;
+}
+
+const cliStyle: Style = {
+  comment: chalk.green,
   keyword: chalk.blue,
   identifier: chalk.green.bold,
-  literal: chalk.yellow.italic
+  literal: chalk.yellow.italic,
+  text: chalk,
+  anchor: chalk,
+  pre: chalk,
+  header: chalk.bold
+};
+
+const htmlStyle: Style = {
+  comment(str) {
+    return `<p class="comment">${str}</p>`;
+  },
+  keyword(str) {
+    return `<p class="keyword">${str}</p>`;
+  },
+  identifier(str) {
+    return `<p class="identifier">${str}</p>`;
+  },
+  literal(str) {
+    return `<p class="literal">${str}</p>`;
+  },
+  text(str) {
+    return str;
+  },
+  anchor(str, href) {
+    return str.anchor(href);
+  },
+  pre(str) {
+    return `<pre>${str}</pre`;
+  },
+  header(str) {
+    return `<h1>${str}<h1>`;
+  }
 };
